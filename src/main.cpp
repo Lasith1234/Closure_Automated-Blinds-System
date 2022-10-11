bcc675fc9a9a76944fb8452a64aff83a55745c83
lasith <lasithraja@gmail.com>
2022-10-11 10:20:12 +0530

    Sensor Initiation and Control

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,62 @@
 #include <Arduino.h>
 #include <BH1750.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
 BH1750 lightMeter(0x23);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+string roomType = "bedroomC";
+
+unordered_map<string, int> room_type;
+
+// room_type["kitchen"] = 400;
+// room_type["office"] = 400;
+// room_type["parking"] = 125;
+// room_type["restroom"] = 200;
+// room_type["stairway"] = 125;
+// room_type["bedroomC"] = 350;
+// room_type["bedroomA"] = 200;
+// room_type["livingroom"] = 300;
+
+void balanceLUX(int recom, float current) {
+  lcd.setCursor(0, 1);
+  if (recom >= current) {
+    lcd.print("UP");
+  } else {
+    lcd.print("DOWN");
+  }
+}
 
 void setup() {
+  // unordered_map<string, int> room_type;
+
+  room_type["kitchen"] = 400;
+  room_type["office"] = 400;
+  room_type["parking"] = 125;
+  room_type["restroom"] = 200;
+  room_type["stairway"] = 125;
+  room_type["bedroomC"] = 350;
+  room_type["bedroomA"] = 200;
+  room_type["livingroom"] = 300;
+
   Serial.begin(9600);
 
   // Initialize the I2C bus (BH1750 library doesn't do this automatically)
   Wire.begin(D2, D1);
 
+  // lcd.begin(16, 2);
+  lcd.init();
+
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  // lcd.print("HELLO");
   /*
 
     BH1750 has six different measurement modes. They are divided in two groups;
@@ -42,6 +89,8 @@ void setup() {
 
   */
 
+  // balanceLUX(roomType);
+
   // begin returns a boolean that can be used to detect setup problems.
   if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)) {
     Serial.println(F("BH1750 Advanced begin"));
@@ -53,8 +102,22 @@ void setup() {
 void loop() {
   if (lightMeter.measurementReady()) {
     float lux = lightMeter.readLightLevel();
-    Serial.print("Light: ");
-    Serial.print(lux);
-    Serial.println(" lx");
+    int balanceV = room_type.at(roomType);
+
+    // Serial.print("Light: ");
+    // Serial.print(lux);
+    // Serial.println(" lx");
+
+    lcd.print("Light: ");
+    lcd.print(lux);
+    lcd.println(" lx");
+
+    lcd.setCursor(0, 1);
+    lcd.print(balanceV);
+
+    balanceLUX(balanceV, lux);
   }
+
+  delay(1000);
+  lcd.clear();
 }
